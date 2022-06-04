5c61714fc28c8c2c0603fe1b6456da5194664e05
tarun nahak <tarunnahak25@gmail.com>
2022-06-04 08:57:55 +0530

    added routes

--- src/router/App.js
+++ src/router/App.js
@@ -1,20 +1,24 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import React from "react";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/login">
-          <Login />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+      <ToastContainer />
+    </>
   );
 }
 
