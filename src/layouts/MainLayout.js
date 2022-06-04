33666f92c7601b01f4b3713e59fcaa6e6c7cff4c
tarun nahak <tarunnahak25@gmail.com>
2022-06-04 23:01:04 +0530

    add the private route

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -8,10 +8,17 @@ const MainLayout = ({ children }) => {
     history.replace("/login");
     window.location.reload(false);
   };
+  const goToPlaces = () => {
+    history.push("/places");
+    window.location.reload(false);
+  };
   return (
     <>
       <Navbar bg="light" variant="light" className="mb-4">
         <Navbar.Brand href="/">QR Menu</Navbar.Brand>
+        <Nav className="flex-grow-1 justify-content-end">
+          <Nav.Link onClick={goToPlaces}>Places</Nav.Link>
+        </Nav>
         <Nav className="flex-grow-1 justify-content-end">
           <Nav.Link onClick={onSignIn}>Login</Nav.Link>
         </Nav>
--- src/router/App.js
+++ src/router/App.js
@@ -1,13 +1,15 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import React from "react";
+import { AuthProvider } from "../contexts/AuthContext";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Places from "../pages/Places";
+import PrivateRoute from "./PrivateRoute";
 
 function App() {
   return (
-    <>
+    <AuthProvider>
       <BrowserRouter>
         <Switch>
           <Route exact path="/">
@@ -16,13 +18,14 @@ function App() {
           <Route exact path="/login">
             <Login />
           </Route>
-          <Route exact path="/places">
+
+          <PrivateRoute exact path="/places">
             <Places />
-          </Route>
+          </PrivateRoute>
         </Switch>
       </BrowserRouter>
       <ToastContainer />
-    </>
+    </AuthProvider>
   );
 }
 
